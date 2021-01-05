847f1af1abbbd3a3c25a7fe1e167b2d6b6d73194
Tobias Hund <tobias.hund@innovation-hub.de>
2021-01-05 06:34:00 +0100

    Base 64 encode image

--- src/main/java/de/innovationhub/prox/professorprofileservice/domain/professor/ProfileImage.java
+++ src/main/java/de/innovationhub/prox/professorprofileservice/domain/professor/ProfileImage.java
@@ -2,15 +2,34 @@ package de.innovationhub.prox.professorprofileservice.domain.professor;
 
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
-import lombok.AllArgsConstructor;
+import javax.persistence.Lob;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.apache.commons.codec.binary.Base64;
+import org.hibernate.annotations.Type;
 
 @Embeddable
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
 public class ProfileImage {
-  @Column(length = 1000, nullable = true)
-  private byte[] data = null;
+  @Lob
+  @Type(type = "org.hibernate.type.BlobType")
+  @Column
+  private byte[] data;
+
+  public ProfileImage(byte[] data) {
+    setData(data);
+  }
+
+  public void setData(byte[] data) {
+    if (!Base64.isBase64(data)) {
+      this.data = Base64.encodeBase64(data);
+    } else {
+      this.data = data;
+    }
+  }
+
+  public byte[] getData() {
+    return Base64.decodeBase64(this.data);
+  }
 }
