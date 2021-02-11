50445435a48b8f1d06943f6897c78e7c8ae5419b
Tobias Hund <tobias.hund@innovation-hub.de>
2021-02-11 08:11:21 +0100

    Add OpenAPI informations

--- src/main/java/de/innovationhub/prox/professorprofileservice/application/config/SpringdocConfig.java
+++ src/main/java/de/innovationhub/prox/professorprofileservice/application/config/SpringdocConfig.java
@@ -1,21 +1,19 @@
 package de.innovationhub.prox.professorprofileservice.application.config;
 
-import com.netflix.discovery.EurekaClient;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.security.SecurityScheme.Type;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springdoc.core.GroupedOpenApi;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SpringdocConfig {
 
-  EurekaClient eurekaClient;
-
-  public SpringdocConfig(@Qualifier("eurekaClient") EurekaClient eurekaClient) {
-    this.eurekaClient = eurekaClient;
-  }
+  public SpringdocConfig() {}
 
   @Bean
   public GroupedOpenApi userOpenApi() {
@@ -33,6 +31,16 @@ public class SpringdocConfig {
         .addServersItem(
             new Server()
                 .url("https://dev.api.prox.innovation-hub.de")
-                .description("PROX Development API"));
+                .description("PROX Development API"))
+        .info(
+            new Info()
+                .title("PROX Professor Profile Service")
+                .description(
+                    "This Service is part of [PROX](https://prox.innovation-hub.de/) and is used to give professors the opportunity to create a custom profile as an informative source about him/her and the projects he/her offers"))
+        .components(
+            new Components()
+                .addSecuritySchemes(
+                    "Bearer",
+                    new SecurityScheme().type(Type.HTTP).scheme("Bearer").bearerFormat("JWT")));
   }
 }
