8b96818db375544a9c93c68acf6a3aba014329ac
tobi6112 <22715034+tobi6112@users.noreply.github.com>
2022-02-15 17:55:50 +0100

    fix: remove obsolet artefacts

--- src/main/java/de/innovationhub/prox/professorprofileservice/ProfessorProfileService.java
+++ src/main/java/de/innovationhub/prox/professorprofileservice/ProfessorProfileService.java
@@ -3,12 +3,10 @@ package de.innovationhub.prox.professorprofileservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
-@EnableEurekaClient
 @EnableSpringDataWebSupport
 @EnableFeignClients
 public class ProfessorProfileService {
